3f4544de9bbc623b9686fd4191f54878c2d4c024
Marcin Rajner <mrajner@pwarch>
2022-11-17 14:03:36 +0100

    ...

---
+++ table.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require 'sequel'
+
+db = Sequel.mysql2("miasta")
+l = 10
+
+miasta = db[:miasta]
+  .where(ifmiasto: true).or(przyszlemiasto: true)
+  .order(:uniqname)
+  .select(:uniqname)
+
+odleglosci = db[:sph]
+
+wybrane = odleglosci
+  .where(fromcity: miasta.limit(l))
+  .where(tocity: miasta.limit(l))
+
+table = db[:miasta]
+  .with(:wybrane, wybrane)
+  .from(:wybrane)
+  .left_join(db[:miasta], {:uniqname => Sequel.qualify(:wybrane, :fromcity)}, :table_alias => :f)
+  .left_join(db[:miasta], {:uniqname => Sequel.qualify(:wybrane, :tocity)}, :table_alias => :t)
+  .select_map([:fromcity, Sequel[:f][:lat], Sequel[:f][:lon], :tocity, Sequel[:t][:lat], Sequel[:t][:lon], :distance])
+  .map{|e|
+    e[0] = e[0].delete(" \t\r\n")
+    e[3] = e[3].delete(" \t\r\n")
+    e.join(' ')
+  }.join("\n")
+
+File.write("table.dat", table)
+
+puts "Brute force will require #{Math.gamma(l).to_i/2} operations"
+
